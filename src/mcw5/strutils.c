818b145f48352df490da74d560cc087d4f6f324f
Paul Giblock <p@pgiblock.net>
2012-06-29 16:19:17 -0400

    Resolve warnings in recently added utils
    Just fix some innocent compile warnings.

--- src/mcw5/strutils.c
+++ src/mcw5/strutils.c
@@ -24,8 +24,14 @@ str_ends_with(const char *str, const char *suffix) {
 
 int
 str_cnt_chr(const char *str, char chr) {
-  int i;
-  for( i = 0; str[i]; str[i] == chr ? ++i : ++str );
+  int i = 0;
+  while( str[i] ) {
+    if( str[i] == chr ) {
+      ++i;
+    } else {
+      ++str;
+    }
+  }
   return i;
 }
 
