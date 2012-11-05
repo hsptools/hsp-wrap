ffb205f3cab272d75341463dfea658ba22c9e098
Paul Giblock <p@pgiblock.net>
2012-11-05 12:09:19 -0500

    Add 'mcwcat' tool for testing mcw/stdiowrap
    Just a really basic tool to cat input to output.  Need to
    add a 'demos' or 'tests' directory, but 'tools' will suffice for now.

---
+++ tools/src/mcwcat.c
@@ -0,0 +1,40 @@
+#define _XOPEN_SOURCE 500
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "stdiowrap/stdiowrap.h"
+
+
+int
+main(int argc, char **argv)
+{
+  int c, i, j;
+  FILE *outf, *inf;
+
+  for (i = 1; i < argc; ++i) {
+    fprintf(stderr, "argv[%d] = %s\n", i, argv[i]);
+  }
+  outf = stdiowrap_fopen(argv[1], "w");
+
+  stdiowrap_fputs("======================\n", outf);
+  for (i = 1; i < argc; ++i) {
+    stdiowrap_fprintf(outf, "argv[%d] = %s\n", i, argv[i]);
+  } 
+  stdiowrap_fputs("======================\n", outf);
+  for (i = 2; i < argc; ++i) {
+    inf = stdiowrap_fopen(argv[i], "r");
+    for (j=0; (c = stdiowrap_fgetc(inf)) != EOF; ++j)
+      stdiowrap_fputc(c, outf);
+    stdiowrap_fclose(inf);
+    stdiowrap_fputs("-----------\n", outf);
+    fprintf(stderr, "File %d. Wrote %d bytes\n", i, j);
+  }
+  stdiowrap_fputs("======================\n", outf);
+
+  sleep(10);
+  stdiowrap_fclose(outf);
+
+  return EXIT_SUCCESS;
+}
